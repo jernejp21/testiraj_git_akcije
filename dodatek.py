a87e633620ae0dc8d90f2d828b232a5a2ab25d55
jernejp21 <jernejp21@tuta.com>
2026-04-15 11:37:30 +0200

    Dodaj python program.

---
+++ dodatek.py
@@ -0,0 +1,8 @@
+#UTF-8
+
+def sestevanje(a :str, b :str) -> int:
+    return a + b
+
+if __name__=="__main__":
+    vsota = sestevanje(5,6)
+    print(vsota)
---
+++ main.py
@@ -0,0 +1,11 @@
+#UTF-8
+
+import dodatek
+
+def main():
+    print("Main funkcija.")
+    vsota = dodatek.sestevanje(10, 30)
+    print(vsota)
+
+if __name__=="__main__":
+    main()
