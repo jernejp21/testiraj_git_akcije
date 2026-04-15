c7c1b620a6a6c258bc13c1db6484519353705d5b
jernejp21 <jernejp21@tuta.com>
2026-04-15 11:57:14 +0200

    Popravil pylint

--- dodatek.py
+++ dodatek.py
@@ -1,8 +1,14 @@
 #UTF-8
+"""
+To je dodatek programa
+"""
 
 def sestevanje(a :str, b :str) -> int:
+    """
+    To je funkcija za seštevanje
+    """
     return a + b
 
 if __name__=="__main__":
-    vsota = sestevanje(5,6)
-    print(vsota)
+    VSOTA = sestevanje(5,6)
+    print(VSOTA)
--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 #UTF-8
+"""
+To je testni program
+"""
 
 import dodatek
 
 def main():
+    """
+    To je začetek programa
+    """
     print("Main funkcija.")
     vsota = dodatek.sestevanje(10, 30)
     print(vsota)
